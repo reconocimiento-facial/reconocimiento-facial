9cbd17c89cd501dcbaf57ad9b5e4a8c35ab098fa
Misael <misaelzurdo@gmail.com>
2018-06-17 21:47:14 -0300

    Separado funcionalidades en archivos

---
+++ Sistema embebido/lib/raspi-utils.js
@@ -0,0 +1,32 @@
+const io = require('socket.io-client');
+const socket = io(config.API);
+const events = require('./lib/events-emitter');
+const status = require('./status');
+
+function printPerson() {
+	lcd.clear();
+	lcd.cursor(row, col);
+	lcd.print(status.person);
+}
+function closeDoor(servo) {
+	 servo.to(180, 500);
+}
+function openDoor(servo, led) {
+	led.pulse();
+	servo.to(0, 500); 
+	setTimeout(() => {
+		led.off();
+		events.emit('door-open');
+	}, 550); 
+}
+function piezoSong(piezo) {
+  piezo.play({
+    song: 'C D F D G - A G - C D F D G - G G G G F F F F - -',
+    beats: 1 / 2,
+    tempo: 5000
+  });
+}
+module.exports = {
+	closeDoor,
+	openDoor
+}
